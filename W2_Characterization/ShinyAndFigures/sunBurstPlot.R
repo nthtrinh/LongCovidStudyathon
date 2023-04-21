4a0d09fdff0f12bec27d8a5606bd61d2d0d33c28
nmercadeb <nmercade147@gmail.com>
2023-04-21 10:39:27 +0200

    more databases

--- W2_Characterization/ShinyAndFigures/sunBurstPlot.R
+++ W2_Characterization/ShinyAndFigures/sunBurstPlot.R
@@ -1,9 +1,14 @@
 ## packages
 library(dplyr)
+devtools::load_all(path = here::here("TreatmentPatterns_code"))
+databaseNames <- c("CPRDGold", "BIGAN", "IPCI")
+
+for (i in 1:length(databaseNames)) {
 
 ## Prepare data
-tp_counts <- read.csv(here::here("data", "CPRDGold_LC any + inf_percentage_groups_treated_noyear.csv"))
-tp_table  <- read.csv(here::here("data", "CPRDGold_LC any + inf_inputsunburst_all.csv"))
+
+tp_counts <- read.csv(here::here("data", paste(databaseNames[i], "_LC_any_inf_percentage_groups_treated_noyear.csv", sep = "")))
+tp_table  <- read.csv(here::here("data", paste(databaseNames[i], "_LC_any_inf_inputsunburst_all.csv", sep = "")))
 
 numOfDrugs <- 10
 
@@ -27,12 +32,12 @@ tpToPlot <- rbind(tp_table[firstDurg %in% drugNames,] %>%
         distinct()) %>%
   mutate(path = gsub("HMG-CoA_inhibitors", "HMG_CoA_inhibitors", .data$path))
 
-devtools::load_all(path = here::here("TreatmentPatterns_code"))
 createSunburstPlot(tpToPlot,
                    folder = here::here(),
-                   fileName = "sunburstPlot")
+                   fileName = databaseNames[i],
+                   title = databaseNames[i ])
 
-
+}
 
 
 
